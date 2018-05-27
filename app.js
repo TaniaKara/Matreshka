b357f7e008695a4eaf6a4374792ce3e1b1896701
shawkyhanana <shawkyhanana@gmail.com>
2018-05-26 21:14:52 -0700

    Adding check for duplication on user auth

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
+const keys = require('./config/keys');
+const mongoose = require("mongoose");
+require('./models/Users');
 require('./services/passport');
 require('./routes/auth')(app);
 require('./config/server');
+
+mongoose.connect(keys.mongoURI);
 
 app.get('/', (req, res, next)=> res.send('Hello World!'));
 app.listen(PORT, ()=> console.log(`Listening on port ${PORT}`));
--- services/passport.js
+++ services/passport.js
@@ -2,13 +2,28 @@ const passport = require('passport');
 const googleStrategy = require("passport-google-oauth20").Strategy;
 const keys = require('../config/keys');
 
+const mongoose = require("mongoose");
+const User = mongoose.model("users");
+
 passport.use(
-    new googleStrategy(
-        {
-          clientID: keys.googleClientID,
-          clientSecret: keys.googleClientSecret,
-          callbackURL: "/auth/google/callback"
-        },
-        (accessToken, refreshToken, profile, done) => {}
-)
+  new googleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    (accessToken, refreshToken, profile, done) => {
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          //existing user
+          console.log('You are an existing user');
+        } else {
+          done(null, existingUser);
+          //Create a new user
+          new User({ googleId: profile.id }).save().then(
+            newUser => done(null, newUser));
+        }
+      });
+    }
+  )
 );
