40bc4ab9fe3b2af3697ab91dc0de887f03eca7ce
shawkyhanana <shawkyhanana@gmail.com>
2018-05-26 21:38:37 -0700

    Adding session cookie

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const app = express();
 const keys = require('./config/keys');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const mongoose = require("mongoose");
 require('./models/Users');
 require('./services/passport');
 require('./routes/auth')(app);
 require('./config/server');
+
+app.use(
+    cookieSession({
+        maxAge: 30*24*60*60*1000,
+        keys: [keys.cookieKey]
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 mongoose.connect(keys.mongoURI);
 
--- services/passport.js
+++ services/passport.js
@@ -5,6 +5,16 @@ const keys = require('../config/keys');
 const mongoose = require("mongoose");
 const User = mongoose.model("users");
 
+passport.serializeUser((newUser, done) => {
+  done(null, newUser.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(cookieUser => {
+    done(null, cookieUser);
+  })
+});
+
 passport.use(
   new googleStrategy(
     {
