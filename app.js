65552f9defcf0a7c0c46b432c72f9d8e1e458aea
shawkyhanana <shawkyhanana@gmail.com>
2018-05-28 13:58:30 -0700

    updates

--- app.js
+++ app.js
@@ -1,25 +1,24 @@
 const express = require('express');
 const app = express();
-const keys = require('./config/keys');
+const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
-const mongoose = require("mongoose");
+const keys = require('./config/keys');
 require('./models/Users');
 require('./services/passport');
 require('./routes/auth')(app);
 require('./config/server');
 
-app.use(
-    cookieSession({
-        maxAge: 30*24*60*60*1000,
-        keys: [keys.cookieKey]
-    })
-);
+mongoose.connect(keys.mongoURI);
 
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
-
-mongoose.connect(keys.mongoURI);
 
 app.get('/', (req, res, next)=> res.send('Hello World!'));
 app.listen(PORT, ()=> console.log(`Listening on port ${PORT}`));
--- services/passport.js
+++ services/passport.js
@@ -1,39 +1,37 @@
 const passport = require('passport');
-const googleStrategy = require("passport-google-oauth20").Strategy;
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
 
-const mongoose = require("mongoose");
-const User = mongoose.model("users");
+const User = mongoose.model('users');
 
-passport.serializeUser((newUser, done) => {
-  done(null, newUser.id);
+passport.serializeUser((user, done) => {
+  done(null, user.id);
 });
 
 passport.deserializeUser((id, done) => {
-  User.findById(id).then(cookieUser => {
-    done(null, cookieUser);
-  })
+  User.findById(id).then(user => {
+    done(null, user);
+  });
 });
 
 passport.use(
-  new googleStrategy(
+  new GoogleStrategy(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: "/auth/google/callback"
+      callbackURL: '/auth/google/callback',
+      proxy: true
     },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id }).then(existingUser => {
-        if (existingUser) {
-          //existing user
-          console.log('You are an existing user');
-        } else {
-          done(null, existingUser);
-          //Create a new user
-          new User({ googleId: profile.id }).save().then(
-            newUser => done(null, newUser));
-        }
-      });
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+
+      if (existingUser) {
+        return done(null, existingUser);
+      }
+
+      const user = await new User({ googleId: profile.id }).save();
+      done(null, user);
     }
   )
 );
