f17d7548452f61fa6d3dc7daf820a2e3c0267830
shawkyhanana <shawkyhanana@gmail.com>
2018-05-30 16:19:11 -0700

    Full auth workflow

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,13 +1,29 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
+  renderContent(){
+    switch (this.props.auth) {
+      case null: 
+        return 'Waiting';
+      case false: 
+         return <li><a href="/auth/google">Login with Google</a></li>;
+      default: 
+      return <li><a href="/api/logout">Logout</a></li>;
+    }
+  }
   render () {
     return (
       <nav>
         <div className='nav-wrapper'>
-          <a href='#' className='brand-logo'>Matreshka</a>
+          <Link 
+          to={this.props.auth ? '/surveys' : '/'} 
+          className='brand-logo'>
+          Matreshka
+          </Link>
           <ul id='nav-mobile' className='right'>
-            <li><a href=''>Login with Google</a></li>
+            {this.renderContent()}
           </ul>
         </div>
       </nav>
@@ -15,4 +31,8 @@ class Header extends Component {
   }
 }
 
-export default Header
+const mapStatetoProps = ({ auth }) => {
+  return { auth };
+}
+
+export default connect(mapStatetoProps)(Header);
